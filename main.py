5a843c17dfee24c3cb0017eda028b822efadc931
Tyler-Carroll <htfu513@gmail.com>
2020-11-14 02:48:50 -0600

    Dropped Columns unnamed columns and renamed label and data

--- main.py
+++ main.py
@@ -13,5 +13,14 @@ nltk.download('stopwords')
 STOPWORDS = set(stopwords.words('english'))
 
 
+#Read Data From CSV
 sms = pd.read_csv("spam.csv", encoding='latin-1')
 print(sms.head())
+
+
+#Drop Undesired Columns, rename columns 1 and 2 to label and message
+sms = sms.drop(['Unnamed: 2', 'Unnamed: 3', 'Unnamed: 4'],axis=1)
+sms = sms.rename(columns={'v1' : 'label', 'v2' : 'message'})
+
+print(sms.groupby('label').describe())
+
