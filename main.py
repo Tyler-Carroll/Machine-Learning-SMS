0593ae46b932fdfaa6cf3394a4d5108427ccaafb
magsz <48229266+magsz@users.noreply.github.com>
2020-11-16 16:00:20 -0600

    trained spam data
    used various machine learning algorithms to train the spam.csv data set and created models to view results

--- main.py
+++ main.py
@@ -24,3 +24,58 @@ sms = sms.rename(columns={'v1' : 'label', 'v2' : 'message'})
 
 print(sms.groupby('label').describe())
 
+# applying a new feature of message length to see if theres anything of interest that we can find
+sms['length'] = sms['message'].apply(len)
+sms.head()
+
+mpl.rcParams['patch.force_edgecolor'] = True
+plt.style.use('seaborn-bright')
+sms.hist(column='length', by='label', bins=50,figsize=(11,5))
+# graph shows that the longer the message the more likely it is spam
+
+text_feat = sms['message'].copy()
+
+def process_Text(text):
+    text = text.translate(str.maketrans('','', string.punctuation))
+    text = [word for word in text.split() if word.lower() not in stopwords.words('english')]
+    
+    return " ".join(text)
+
+text_feat = text_feat.apply(process_Text)
+
+vectorizer = TfidfVectorizer("english")
+
+features = vectorizer.fit_transform(text_feat)
+
+features_training_Model, features_test_Model, labels_train_Model, labels_test_Model = train_test_split(features, sms['label'], test_size=0.3, random_state=111)
+
+# importing ML algorithm libraries
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import BaggingClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+
+log_Reg = LogisticRegression(solver='liblinear', penalty="l1")
+rfc = RandomForestClassifier(n_estimators=31, random_state=111)
+bc = BaggingClassifier(n_estimators=9, random_state=111)
+dtc = DecisionTreeClassifier(min_samples_split=7, random_state=111)
+
+clfs = { 'DT': dtc, 'LR': log_Reg, 'RF': rfc,'BgC': bc}
+
+def train_classifier(clf, feature_train, labels_train):    
+    clf.fit(feature_train, labels_train)
+
+def predict_labels(clf, features):
+    return (clf.predict(features))
+
+pred_scores = []
+
+for k,v in clfs.items():
+    train_classifier(v, features_training_Model, labels_train_Model)
+    pred = predict_labels(v,features_test_Model)
+    pred_scores.append((k, [accuracy_score(labels_test_Model,pred)]))
+
+df = pd.DataFrame.from_dict(dict(pred_scores),orient='index', columns=['Score'])
+df
+
